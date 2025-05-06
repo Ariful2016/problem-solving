7585124d8b722960699d5a49dd79f43954d96f65
Ariful2016 <arifewucse2016@gmail.com>
2025-05-06 12:21:19 +0600

    solved

--- src/array/TwoSum.kt
+++ src/array/TwoSum.kt
@@ -1,29 +1,49 @@
 package array
 
 fun main() {
+    // Define an array of integers
     val num = intArrayOf(1, 3, 7, 9, 2)
+
+    // Call the twoSum function with the array and target value 11
     val result = twoSum(num, 11)
 
+    // Check if a valid result (non-empty array) was returned
     if (result.isNotEmpty()) {
+        // Print the indices of the two numbers that add up to the target
         println("Indices: ${result[0]}, ${result[1]}")
     } else {
+        // No matching pair was found
         println("No match found")
     }
 }
 
+/**
+ * Function to find two indices in the array whose values add up to the given target.
+ *
+ * @param num The array of integers to search within.
+ * @param target The target sum we are looking for.
+ * @return An IntArray containing the two indices that satisfy the condition, or an empty array if none found.
+ */
 fun twoSum(num: IntArray, target: Int): IntArray {
-    val hm = mutableMapOf<Int, Int>() // value to index
+    // Create a HashMap to store numbers and their corresponding indices
+    val hm = mutableMapOf<Int, Int>() // key: number, value: index
 
+    // Iterate over the array using indices
     for (i in num.indices) {
-        if (hm.containsKey( target - num[i])) {
-            return intArrayOf(hm[ target - num[i]]!!, i)
+        // Check if the target - num[i] exists in the map
+        if (hm.containsKey(target - num[i])) {
+            // If found, return the index of the target - num[i] and the current index
+            return intArrayOf(hm[target - num[i]]!!, i)
         } else {
+            // Otherwise, store the current number with its index in the map
             hm[num[i]] = i
         }
     }
 
-    return intArrayOf() // return empty array if no match
+    // If no such pair is found, return an empty array
+    return intArrayOf()
 }
+
 
 
 /**
