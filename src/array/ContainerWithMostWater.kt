e59ab1e39000dfe8b0bf2e91ebd5bb4a75e8ed9a
Ariful2016 <arifewucse2016@gmail.com>
2025-05-12 12:29:28 +0600

    leetcode problem 11 solution optimized

--- src/array/ContainerWithMostWater.kt
+++ src/array/ContainerWithMostWater.kt
@@ -3,6 +3,31 @@ package array
 import kotlin.math.max
 import kotlin.math.min
 
+/**
+ * 11. Container With Most Water
+ * Medium
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: height = [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+ * Example 2:
+ *
+ * Input: height = [1,1]
+ * Output: 1
+ */
+
 fun main() {
     // Sample input array representing heights of vertical lines
     val heights = intArrayOf(7, 1, 2, 9, 2)
@@ -64,27 +89,3 @@ fun maxArea(height: IntArray): Int {
     return maxA
 }
 
-/**
- * 11. Container With Most Water
- * Medium
- * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
- *
- * Find two lines that together with the x-axis form a container, such that the container contains the most water.
- *
- * Return the maximum amount of water a container can store.
- *
- * Notice that you may not slant the container.
- *
- *
- *
- * Example 1:
- *
- *
- * Input: height = [1,8,6,2,5,4,8,3,7]
- * Output: 49
- * Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
- * Example 2:
- *
- * Input: height = [1,1]
- * Output: 1
- */
